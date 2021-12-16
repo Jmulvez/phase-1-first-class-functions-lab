7058e14e51a72996707e1901163e03e3b9ab317c
John <johnmulvey48@yahoo.com>
2021-12-16 11:19:08 -0500

    git@github.com:Jmulvez/phase-1-first-class-functions-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers('firstDrivers');
+const returnlastTwoDrivers('lastDrivers');
+const fareDoubler('fareTimesTwo');
+const fareTripler('fareTimesThree');
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const createFareMultiplier('fare' * "x");
+
+const function(function('returnFirstTwoDrivers')); {
+     console.log('returnFirstTwoDrivers');
+}
+
+const function(function('returnLastTwoDrivers')); {
+    console.log('returnLastTwoDrivers');
+}
+
+const function('selectingDrivers'()); {
+    console.log('returnFirstTwoDrivers' + "returnLastTwoDrivers");
+}
+
+const function('createFareMultiplier'); {
+    console.log(function('createFareMultiplier' * '4');
+}
+
+const function('selectDifferentDrivers'); {
+    console.log('returnFirstTwoDrivers', 'returnLastTwoDrivers');
+}
